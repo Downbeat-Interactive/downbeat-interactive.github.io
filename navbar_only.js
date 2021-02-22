ce3389a9ef362d9ee458c2b773ced39845862992
Jackson Dean <jackson@jacksonsdean.com>
2021-02-22 13:30:12 -0500

    updating navbar

---
+++ navbar_only.js
@@ -0,0 +1,11 @@
+'use strict';
+
+import { NavigationMenu, Header } from "./common_components.js";
+
+var domContainer = document.querySelector('#app_container');
+ReactDOM.render(React.createElement(
+    'div',
+    null,
+    React.createElement(Header, null),
+    React.createElement(NavigationMenu, null)
+), domContainer);
---
+++ src/navbar_only.js
@@ -0,0 +1,7 @@
+'use strict';
+
+import { NavigationMenu, Header } from "./common_components.js";
+
+let domContainer = document.querySelector('#app_container');
+ReactDOM.render(<div><Header/>
+    <NavigationMenu/></div>, domContainer);
