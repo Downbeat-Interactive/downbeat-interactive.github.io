b7817f6a67faa6cbcfed37fa36a01ba0246fc52c
Jackson Dean <jackson@jacksonsdean.com>
2021-05-26 17:07:01 -0400

    adding missing files from last commit

--- game_data.js
+++ game_data.js
@@ -1,28 +1,31 @@
-export default [{
-    id: 'default',
-    title: '',
-    subtitle: '',
-    bannerImg: '',
+export default [
+  {
+    id: "default",
+    title: "",
+    subtitle: "",
+    bannerImg: "",
     url: undefined,
     nScreens: 0,
-    videoUrl: '',
-    storeUrl: '',
-    bundle: '',
-    releaseDate: '',
+    videoUrl: "",
+    storeUrl: "",
+    bundle: "",
+    releaseDate: "",
     videoDimensions: [undefined, undefined],
-    description: ''
-}, {
-    id: 'dandy-drift',
-    title: 'Dandy Drift',
-    subtitle: 'Relaxing Runner',
-    bannerImg: '',
-    url: '/dandy-drift',
+    description: "",
+  },
+  {
+    id: "dandy-drift",
+    title: "Dandy Drift",
+    subtitle: "Relaxing Runner",
+    bannerImg: "",
+    url: "/dandy-drift",
     nScreens: 0,
-    videoUrl: 'https://www.youtube.com/embed/ALo7LZK3vUU',
+    videoUrl: "https://www.youtube.com/embed/ALo7LZK3vUU",
     videoDimensions: [undefined, undefined],
-    bundle: 'com.Jackson.DandyDrift',
-    releaseDate: 'November 2020',
-    description: '<div class="feature-list">\
+    bundle: "com.Jackson.DandyDrift",
+    releaseDate: "November 2020",
+    description:
+      '<div class="feature-list">\
                             Dandy Drift is a casual game about drifting on a river.<br/><br/>\
                             <ul className="align-left">\
                                 <li key="item0">Avoid obstacles &amp; see how far you can go</li>\
@@ -31,74 +34,126 @@ export default [{
                                 <li key="item3">Jam to the cool beats from Abstraction</li>\
                                 <li key="item4">Take a break and drift!</li>\
                             </ul> \
-                        </div>'
-}, {
-    id: 'road-trip-rush',
-    title: 'Road Trip Rush',
-    subtitle: 'Frantic Dash',
-    bannerImg: '',
-    url: '/road-trip-rush',
+                        </div>',
+  },
+  {
+    id: "road-trip-rush",
+    title: "Road Trip Rush",
+    subtitle: "Frantic Dash",
+    bannerImg: "",
+    url: "/road-trip-rush",
     nScreens: 5,
-    videoUrl: 'https://www.youtube.com/embed/eqQxFkQ41Bo',
-    bundle: 'com.Jackson.DebrisDash',
-    releaseDate: 'January 2021',
+    videoUrl: "https://www.youtube.com/embed/eqQxFkQ41Bo",
+    bundle: "com.Jackson.DebrisDash",
+    releaseDate: "January 2021",
     videoDimensions: [560, 315],
-    description: 'You\'re rushing down the highway at top speed with no control. Frantically drag obstacles out of the road before you crash!\
-            <br/><br/>In Road Trip Rush, you don\'t control the car. Instead, you have to move physically-simulated debris out of the road by dragging and flicking.\
-        '
-}, {
-    id: 'parkour-run',
-    title: 'Parkour Run',
-    subtitle: 'Precise Action',
-    bannerImg: '',
-    url: '/parkour-run',
+    description:
+      "You're rushing down the highway at top speed with no control. Frantically drag obstacles out of the road before you crash!\
+            <br/><br/>In Road Trip Rush, you don't control the car. Instead, you have to move physically-simulated debris out of the road by dragging and flicking.\
+        ",
+  },
+  {
+    id: "parkour-run",
+    title: "Parkour Run",
+    subtitle: "Precise Action",
+    bannerImg: "",
+    url: "/parkour-run",
     nScreens: 4,
-    videoUrl: 'https://youtube.com/embed/gjkYDLshdtU',
+    videoUrl: "https://youtube.com/embed/gjkYDLshdtU",
     videoDimensions: [undefined, undefined],
-    bundle: 'com.downbeat.swiperun',
-    releaseDate: 'February 2021',
-    description: 'Swipe obstacles out of the way as you sprint along the rooftops!\
+    bundle: "com.downbeat.swiperun",
+    releaseDate: "February 2021",
+    description:
+      "Swipe obstacles out of the way as you sprint along the rooftops!\
         <br/>Can you make it to the goal?\
-        '
-}, {
-    id: 'tactics-rush',
-    title: 'Tactics Rush',
-    subtitle: 'Strategic Puzzler',
-    bannerImg: '',
-    url: '/tactics-rush',
+        ",
+  },
+  {
+    id: "tactics-rush",
+    title: "Tactics Rush",
+    subtitle: "Strategic Puzzler",
+    bannerImg: "",
+    url: "/tactics-rush",
     nScreens: 8,
-    videoUrl: 'https://youtube.com/embed/J0CLX6Axdcs',
+    videoUrl: "https://youtube.com/embed/J0CLX6Axdcs",
     videoDimensions: [undefined, undefined],
-    storeUrl: 'https://play.google.com/store/apps/details?id=',
-    bundle: 'com.downbeat.tacticsrush',
-    releaseDate: 'February 2021',
-    description: 'Gather your troops and try to capture the enemy bunker.<br/><br/>\
-        Can you get the high score?'
-}, {
-    id: 'balance-run',
-    title: 'Balance Run',
-    subtitle: 'Intense focus',
-    bannerImg: '',
-    url: '/balance-run',
+    storeUrl: "https://play.google.com/store/apps/details?id=",
+    bundle: "com.downbeat.tacticsrush",
+    releaseDate: "February 2021",
+    description:
+      "Gather your troops and try to capture the enemy bunker.<br/><br/>\
+        Can you get the high score?",
+  },
+  {
+    id: "balance-run",
+    title: "Balance Run",
+    subtitle: "Intense focus",
+    bannerImg: "",
+    url: "/balance-run",
     nScreens: 5,
-    videoUrl: 'https://youtube.com/embed/jrMK7StJakE',
+    videoUrl: "https://youtube.com/embed/QE5_AcfeJ7E",
     videoDimensions: [undefined, undefined],
-    bundle: 'com.downbeat.polebalance',
-    releaseDate: 'January 2021',
-    description: 'Keep yourself balanced by moving blocks left or right as you run high above the ground. \
-        <br/>Try not to fall!'
-}];
+    bundle: "com.downbeat.polebalance",
+    releaseDate: "January 2021",
+    description:
+      "Keep yourself balanced by moving blocks left or right as you run high above the ground. \
+        <br/>Try not to fall!",
+  },
+  {
+    id: "guess-that-phrase",
+    title: "Guess that Phrase!",
+    subtitle: "Game show puzzler",
+    bannerImg: "",
+    url: "/guess-that-phrase",
+    nScreens: "7",
+    videoUrl: "https://youtube.com/embed/zwjDmGGek4k",
+    videoDimensions: [undefined, undefined],
+    bundle: "com.downbeat.hangman",
+    releaseDate: "March 2021",
+    description:
+      "Compete in a game-show where you try to guess missing letters from common phrases. Can you outsmart your opponent?",
+  },
+  {
+    id: "fbi-guy",
+    title: "FBI Guy",
+    subtitle: "",
+    bannerImg: "",
+    url: "/fbi-guy",
+    nScreens: "8",
+    videoUrl: "https://youtube.com/embed/qu371G7u7BM",
+    videoDimensions: [undefined, undefined],
+    bundle: "com.downbeat.fbiguy",
+    releaseDate: "April 2021",
+    description:
+      "Be an FBI Guy who hacks into people's phones and improves their text messages before they're sent.",
+  },
+  {
+    id: "spell-hero",
+    title: "Spell Hero",
+    subtitle: "Be the hero",
+    bannerImg: "",
+    url: "/spell-hero",
+    nScreens: "7",
+    videoUrl: "https://youtube.com/embed/jrMK7StJakE",
+    videoDimensions: [undefined, undefined],
+    bundle: "com.downbeat.spellhero",
+    releaseDate: "May 2021",
+    description:
+      "Control the elements to solve problems and be a hero!<br/>As seen on GamesKeys: <a target='_blank' href='https://gameskeys.net/top-android-games-to-tryout-in-may-2021/'>Top Android Games to Try in May 2021<a/>",
+  },
+];
 
 export var comingSoonData = {
-    id: 'coming-soon',
-    title: 'Coming Soon',
-    subtitle: 'Check back later',
-    bannerImg: '',
-    url: '',
-    nScreens: 0,
-    videoUrl: '',
-    videoDimensions: [undefined, undefined],
-    bundle: '',
-    releaseDate: '',
-    description: 'We\'re working on new projects all the time, come back in a bit to see what\'s new!'
+  id: "coming-soon",
+  title: "Coming Soon",
+  subtitle: "Check back later",
+  bannerImg: "",
+  url: "",
+  nScreens: 0,
+  videoUrl: "",
+  videoDimensions: [undefined, undefined],
+  bundle: "",
+  releaseDate: "",
+  description:
+    "We're working on new projects all the time, come back in a bit to see what's new!",
 };
